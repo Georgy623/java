e21f12fc5c7cbd104d67dcd6b8dc1bdcd4a8a176
Georgy623 <90360245+Georgy623@users.noreply.github.com>
2023-03-09 11:39:06 -0600

    Create CountyByFives.java

---
+++ chapter6/CountyByFives.java
@@ -0,0 +1,14 @@
+public class CountByFives {
+    public static void main (String args[]) {
+        // Write your code here
+        for (int i = 0; i < 500;) {
+            i=i+5;
+  System.out.println(" "+i);
+
+  if (i%50==0){
+      System.out.println("\r");
+
+  }
+}
+    }
+}
